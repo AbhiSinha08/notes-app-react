424244cabc0f4d17f815ff9a1fa23055f1ddbd2a
Abhinav <imonline.abhinav@gmail.com>
2022-07-13 15:47:33 +0530

    Notes app part 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*/**/.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        "sans-serif": "Dosis, sans-serif",
+        "head": "Kalam, sans-serif",
+        "body": "Edu TAS Beginner, cursive"
+      },
+      colors: {
+        
+      }
     },
   },
   plugins: [],
